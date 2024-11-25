0738f820a18ed950a5f9f70cf9e4b60f91e907f9
AWieclawski <a_wieclawski@poczta.pl>
2024-11-25 18:30:11 +0100

    Init2

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-
+# https://www.baeldung.com/docker-maven-build-multi-module-projects
 echo "About to build docker using Dockerfile in ${PWD}" 
-docker build -t sniffer-jdk11 .
+docker build -t jdk11-sniffer:from-dockerfile .
 echo "About to run docker as detached container" 
-docker run -d --name sniffer  -p 3333:8080 sniffer-jdk11
+docker run -d --name sniffer_3456 -p 3456:8080 jdk11-sniffer:from-dockerfile
 echo "Presentation of active dockers"
 sudo docker ps
 echo "Sniffer WeClient available at: http://localhost:3333/dtos"
